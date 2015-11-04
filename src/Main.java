e775cd31f4e0649e73268c2036a29d963b5bc8c0
herrtill <tenorhorn7@gmail.com>
2015-11-04 11:51:24 +0100

    news

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Main {
+    public Main() {
+        try {
+            //hello
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+
+            while(!Display.isCloseRequested()) {
+                Display.update();
+            }
+
+            Display.destroy();
+        } catch(LWJGLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
